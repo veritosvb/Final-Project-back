288fde8688b6658b8c2a717f3e56dd230eb8715e
Veritosvb <vero.velez@gmail.com>
2018-09-20 21:22:37 -0500

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-require('dotenv').config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
